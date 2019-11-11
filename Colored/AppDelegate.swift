7136db5ed7b1e5383c8fca3011c762e89ec642bd
Martin Lexow <martin.lexow@ixeau.com>
2019-11-11 21:33:57 +0100

    Initial Commit

---
+++ Colored/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Colored
+//
+//  Created by Martin Lexow on 19-11-11.
+//  Copyright © 2019 IXEAU. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Colored/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Colored
+//
+//  Created by Martin Lexow on 19-11-11.
+//  Copyright © 2019 IXEAU. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
